d1945eb902946439336f575e9cb69a9a4b990e9d
Maria Emilia <mariaemilia.rodrigues@estudante.iftm.edu.br>
2025-09-22 21:27:46 -0300

    Finaliza CRUD Usuario, implementando bcrypt e auth

--- src/modules/usuario/usuario.routes.ts
+++ src/modules/usuario/usuario.routes.ts
@@ -12,5 +12,7 @@ router.get("/id/:id", authMiddleware, usuarioController.findById);
 router.get("/nome/:nome", authMiddleware, usuarioController.findByNome);
 router.get("/cpf/:cpf", authMiddleware, usuarioController.findByCpf);
 router.get("/email/:email", authMiddleware, usuarioController.findByEmail);
+router.put("/:id", authMiddleware, usuarioController.update);
+router.delete("/:id", authMiddleware, usuarioController.delete);
 
 export default router;
