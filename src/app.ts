6a532198375889345fdb3b5ae64373e6e0df348b
igsem123 <136397682+igsem123@users.noreply.github.com>
2025-10-04 15:18:01 -0300

    PC-49 Feature: reorganiza importações e ajusta configuração das rotas no app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,19 @@ import animalRoutes from "./modules/animal/animal.routes";
 import authRoutes from "./modules/auth/auth.routes";
 import tipoVacinaRoutes from "./modules/vacina/tipoVacina.routes";
 import aplicacaoVacinaRoutes from "./modules/vacina/aplicacaoVacina.routes";
-import { errorHandler } from "./middlewares/errorHandler";
-import { prisma } from "./config/prisma";
-import usuarioRoutes from "./modules/usuario/usuario.routes";
-import cors from "cors";
 import tipoRacaRoutes from "./modules/tipoRaca/tipoRaca.routes";
-import "./shared/container"; // Importa as configurações do container de injeção de dependências
+import usuarioRoutes from "./modules/usuario/usuario.routes";
 import cadastroRoutes from "./routes/cadastro.route";
+import {errorHandler} from "./middlewares/errorHandler";
+import {prisma} from "./config/prisma";
+import cors from "cors";
+import "./shared/container"; // Importa as configurações do container de injeção de dependências
 
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3001",
-  credentials: true,
+    origin: process.env.FRONTEND_URL || "http://localhost:3001",
+    credentials: true,
 }));
 
 app.use(express.json());
@@ -26,20 +26,20 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use("/api/cadastro", cadastroRoutes);
 
 // Verifica conexão
 app.get("/", async (req, res) => {
-  try {
-    await prisma.$connect();
-    res.send("CowUai Backend funcionando e banco de dados conectado! ✅");
-  } catch (error) {
-    res
-      .status(500)
-      .send("CowUai Backend funcionando, mas com erro no banco de dados. ❌");
-  }
+    try {
+        await prisma.$connect();
+        res.send("CowUai Backend funcionando e banco de dados conectado! ✅");
+    } catch (error) {
+        res
+            .status(500)
+            .send("CowUai Backend funcionando, mas com erro no banco de dados. ❌");
+    }
 });
 
+app.use("/api/cadastro", cadastroRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/usuarios", usuarioRoutes);
 app.use("/api/fazendas", fazendaRoutes);
